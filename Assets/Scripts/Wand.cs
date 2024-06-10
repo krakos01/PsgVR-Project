f2f567a0b90aeff3f183116f7c789e58a9887b81
krakos <62389231+krakos01@users.noreply.github.com>
2024-06-10 13:59:10 +0200

    ControllerShoot

--- Assets/Scripts/Wand.cs
+++ Assets/Scripts/Wand.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Wand : MonoBehaviour
 {
     private int ammoRecoveryTime = 1;  // How much seconds pass every 1 ammo recover; 1 sec may be to slow
-    
+    private InputAction triggerAction;
+
     public int playerAmmo = 3;
     public GameObject projectilePrefab;
     public Transform projectileSpawnPoint;
-
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("RecoverProjectile", 0, ammoRecoveryTime);
+
+        // Inicjaizacja InputAction dla triggera
+        var inputActionAsset = GetComponent<PlayerInput>().actions;
+        triggerAction = inputActionAsset.FindAction("Fire");
+        triggerAction.performed += _ => LaunchProjectile();
+    }
+
+
+    private void OnDestroy()
+    {
+        triggerAction.performed -= _ => LaunchProjectile();
     }
 
     // Update is called once per frame
     void Update()
     {
-        LaunchProjectile();
+        //LaunchProjectile();
     }
 
+ 
+    protected virtual void StartShooting(XRBaseInteractor interactor) { }
 
     /// <summary>
     ///  Create projectile at user's position and launch when pressend LMB
