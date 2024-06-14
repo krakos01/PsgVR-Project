64e839afcc0c532eb06fb8d31eb2e3d69406fcfb
Loukien <107815829+Loukien@users.noreply.github.com>
2024-06-14 18:37:25 +0200

    add boom audio and better boom vfx

--- Assets/Scripts/EnemyShoot.cs
+++ Assets/Scripts/EnemyShoot.cs
@@ -32,7 +32,7 @@ public class EnemyShoot : MonoBehaviour
     {
         if (player != null)
         {
-            shootDirection = (player.transform.position - transform.position - new Vector3(0, 0.5f, 0)).normalized;
+            shootDirection = (player.transform.position - transform.position - new Vector3(0, 0, 0)).normalized;
         }
     }
 
